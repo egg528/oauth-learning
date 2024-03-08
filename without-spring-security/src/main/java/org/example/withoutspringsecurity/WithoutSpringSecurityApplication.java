256ae44306114dfd8ade3d8b10b0b82f21082005
wkwon <wkwon@ebay.com>
2024-03-09 00:34:26 +0900

    Initial commit

---
+++ without-spring-security/src/main/java/org/example/withoutspringsecurity/WithoutSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.withoutspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithoutSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithoutSpringSecurityApplication.class, args);
+    }
+
+}
